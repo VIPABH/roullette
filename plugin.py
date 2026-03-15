2dadb7bc6ba615c17df67da87e1197fb49de73b6
VIPABH <a891983.2@gmail.com>
2026-03-16 01:11:48 +0300

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -9,15 +9,8 @@ async def start(e):
     uid = e.sender_id
     ment = await mention(e)
     if uid in name: 
-        buttons = [
-            [Button.inline("تعيين قناة اشتراك إجباري", data="set_channel"),
-             Button.inline("حذف قناة اشتراك إجباري", data="del_channel")],
-            [Button.inline('عرض قنوات الاشتراك', data="show_channels"),
-             Button.inline('عدد المستخدمين', data="count_users")],
-            [Button.inline('الإعدادات', data='settings')]
-        ]
         caption = f'اهلا عزيزي ({ment})، أنت حالياً في واجهة الآدمن. ماذا تريد أن تفعل؟ 👇🏾'
-        await e.reply(caption, buttons=buttons)        
+        await main_settings(e, caption)        
     else:
         results = await asyncio.gather(
             *(is_in_channel(uid, ch) for ch in channels)
