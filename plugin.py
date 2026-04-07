440595783fdfe7152c4009a7c161bee0eeb2bbed
VIPABH <a891983.2@gmail.com>
2026-04-07 11:41:26 +0300

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -5,7 +5,6 @@ import asyncio
 channels = r.smembers("forced_channels")
 @ABH.on(events.NewMessage(pattern="^/start$"))
 async def start(e):
-    r.delete("banned_users")
     if r.sismember("banned_users", str(e.sender_id)):
         raise events.StopPropagation 
     if not e.is_private:
